f8cdd707ab12cc5eee06f20572a8045da5fcd99b
Aditya Sharma <sharmaaditya5701913@gmail.com>
2019-11-09 13:46:39 +0530

    a

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const jsonfile = require('jsonfile');
-
 const moment = require('moment');
+const simpleGit = require('simple-git');
+const random = require('random');
 
 const FILEPATH = './data.json';
 
-const DATE = moment().format();
-
-const data = {
-    date: DATE
+const makeCommit = n => {
+    if(n === 0) simpleGit().push();
+    const x = random.int(0, 54);
+    const y = random.int(0, 6);
+    const DATE = moment().subtract(1, 'y').add(1, 'd').add(x, 'w').add(y, 'd').format();
+    const data = {
+        date: DATE
+    }
+    jsonfile.writeFile(FILEPATH, data, () => {
+        simpleGit().add([FILEPATH]).commit(DATE, {'--date': DATE})
+        makeCommit(--n);
+    });
 }
-
-jsonfile.writeFile(FILEPATH, data);
+makeCommit(2);
